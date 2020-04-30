85a6ffedd6a88bb10bf5a3382c489bd5209a6f01
j9mbo <62254098+j9mbo@users.noreply.github.com>
2020-04-30 18:15:37 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,5 @@
 provider "aws" {
-  region     = "us-west-2"
-  access_key = "AKIAIYJKRVICFLWB2BQA"
-  secret_key = "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"
-    
+
 }
 
 data "aws_availability_zones" "all" {}
